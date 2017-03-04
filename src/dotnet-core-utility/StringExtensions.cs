5568da8ef28bb0a0aba7cf77e45020439253f45d
Jonas Wirth <jonas-wirth@gmx.ch>
2017-03-04 11:26:38 +0100

    Added string extensions

---
+++ src/dotnet-core-utility/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_core_utility
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks if a string is empty or null
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            if (str == null || str.Trim() == "")
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// Checks if a string is empty, null or consists only of whitespaces
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhitespace(this string str)
+        {
+            if (str.Trim().IsNullOrEmpty())
+                return true;
+            return false;
+        }
+    }
+}
