dedb6c790bd6320323f1b5b56e5329dfb6d383f1
Jonas Wirth <jonas-wirth@gmx.ch>
2017-03-05 12:24:45 +0100

    Added new extension methods

---
+++ src/dotnet-core-utility/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_core_utility
+{
+    public static class ListExtensions
+    {
+        public static List<string> ToLower(this List<string> list)
+        {
+            List<string> returnList = new List<string>();
+            for(int i = 0; i < list.Count; i++)
+            {
+                returnList.Add(list.ElementAt(i).ToLower());
+            }
+            return returnList;
+        }
+    }
+}
--- src/dotnet-core-utility/StringExtensions.cs
+++ src/dotnet-core-utility/StringExtensions.cs
@@ -14,7 +14,7 @@ namespace dotnet_core_utility
         /// <returns></returns>
         public static bool IsNullOrEmpty(this string str)
         {
-            if (str == null || str.Trim() == "")
+            if (str == null || str == "")
                 return true;
             return false;
         }
@@ -27,6 +27,31 @@ namespace dotnet_core_utility
         {
             if (str.Trim().IsNullOrEmpty())
                 return true;
+            return false;
+        }
+        /// <summary>
+        /// Checks if a string contains any of the strings passed in the IEnummerable
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="list"></param>
+        /// <param name="caseSensitive"></param>
+        /// <returns></returns>
+        public static bool ContainsAny(this string str, IEnumerable<string> list, bool caseSensitive = true)
+        {
+            string testStr = str;
+            List<string> values = list.ToList();
+
+            if (!caseSensitive)
+            {
+                testStr = testStr.ToLower();
+                values = values.ToLower();
+            }
+
+            foreach(var val in values)
+            {
+                if (testStr.Contains(val))
+                    return true;
+            }
             return false;
         }
     }
