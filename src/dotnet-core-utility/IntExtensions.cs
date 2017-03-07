c900c4ac5b13fddd92a4c634434c8911a649338d
Jonas Wirth <jonas-wirth@gmx.ch>
2017-03-07 20:26:40 +0100

    Added int extensions

---
+++ src/dotnet-core-utility/IntExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_core_utility
+{
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int value)
+        {
+            return (value % 2 == 0);
+        }
+
+        public static bool IsTheAnswerToLifeTheUniverseAndEverything(this int value)
+        {
+            return (value == 42);
+        }
+    }
+}
