4792dc223b20f9f017810e76cb1847050a0d1048
Jonas Wirth <jonas-wirth@gmx.ch>
2017-04-18 11:03:36 +0200

    Added two new list extensions

--- src/dotnet-core-utility/ListExtensions.cs
+++ src/dotnet-core-utility/ListExtensions.cs
@@ -7,6 +7,11 @@ namespace dotnet_core_utility
 {
     public static class ListExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
         public static List<string> ToLower(this List<string> list)
         {
             List<string> returnList = new List<string>();
@@ -15,6 +20,32 @@ namespace dotnet_core_utility
                 returnList.Add(list.ElementAt(i).ToLower());
             }
             return returnList;
+        }
+        /// <summary>
+        /// Returns a specific page of a list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static List<T> GetPageOfList<T>(this IEnumerable<T> list, int pageIndex, int pageSize)
+        {
+            int startIndex = (pageIndex - 1) * pageSize;
+            return list.Range(startIndex, pageSize * pageIndex);
+        }
+        /// <summary>
+        /// Returns a range of an IEnummerable as list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static List<T> Range<T>(this IEnumerable<T> list, int start, int end)
+        {
+            int count = end - start;
+            return list.ToList<T>().GetRange(start, count);
         }
     }
 }
